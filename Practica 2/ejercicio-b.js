68ec121be965a41224feb64dbc883a68d8dd416f
Gael <79112826+EchoMeter4@users.noreply.github.com>
2025-09-19 15:19:30 -0600

    practica 2: ejercicio b.

---
+++ Practica 2/ejercicio-b.js
@@ -0,0 +1,19 @@
+console.log('Ejercicio B');
+
+const productos = [
+    {nombre: "Laptop", precio: 12000},
+    {nombre: "Mouse", precio: 250},
+    {nombre: "Teclado", precio: 750},
+    {nombre: "Monitor", precio: 3000}
+];
+
+const filtered = productos.filter(
+    producto => producto.precio > 1000
+);
+
+const nombres = productos.map(
+    producto => producto.nombre
+);
+
+console.log(nombres);
+console.log(filtered);
