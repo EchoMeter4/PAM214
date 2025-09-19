9f14a81763c0f3f6c4ff6688f1f0c94183d1d9ab
Gael <79112826+EchoMeter4@users.noreply.github.com>
2025-09-19 15:19:25 -0600

    practica 2: ejercicio a.

---
+++ Practica 2/ejercicio-a.js
@@ -0,0 +1,14 @@
+console.log('Ejercicio A');
+
+const persona = {
+    nombre: "Ivan Isay",
+    edad: 37,
+    direccion: {
+        ciudad: "Qro",
+        pais: "MX"
+    }
+};
+
+const {nombre, edad, direccion: {ciudad}} = persona;
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en Qro.`);
