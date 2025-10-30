9a68d2fdfb5138027fe7767e4077f66511be4f2a
Gael <79112826+EchoMeter4@users.noreply.github.com>
2025-10-30 17:05:05 -0600

    ActivityIndicator: Completada

--- intro/screens/ActivityIndicatorScreen.js
+++ intro/screens/ActivityIndicatorScreen.js
@@ -1,19 +1,54 @@
-import {Text, StyleSheet, View, Button} from "react-native";
 import React, {useState} from 'react';
+import {View, Text, Button, ActivityIndicator, StyleSheet} from 'react-native';
 
-export default function ActivityIndicatorScreen() {
+export default function ActivityScreen() {
+    const [loading, setloading] = useState(false);
+
+    const iniciarCarga = () => {
+        setloading(true);
+        setTimeout(() => setloading(false), 3000);
+    };
+
     return (
-        <View>
-            <Text>Próximamente</Text>
+        <View style={styles.container}>
+            <Text style={styles.texto}>Presiona para iniciar carga</Text>
+
+            <ActivityIndicator
+                style={styles.indicador}
+                animating={loading}
+                size="large"
+                color="#007BFF"
+                hidesWhenStopped={true}
+            />
+
+            <View style={styles.botonContainer}>
+                <Button
+                    color="red" title="Iniciar carga" onPress={iniciarCarga}
+                />
+            </View>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F8F9FA',
+    },
+    texto: {
+        fontFamily: 'Times New Roman',
+        marginBottom: 20,
+        fontSize: 18,
+        color: '#333',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+    },
+    indicador: {
+        marginVertical: 10,
+    },
+    botonContainer: {
+        marginTop: 30,
     },
 });
