1ce3d12d6e4b07e700662334ce3e9ece23733065
Gael <79112826+EchoMeter4@users.noreply.github.com>
2025-10-30 16:34:41 -0600

    Add buttons assignment.

--- intro/screens/BotonesScreen.js
+++ intro/screens/BotonesScreen.js
@@ -1,12 +1,72 @@
-import {Text, StyleSheet, View, Button} from "react-native";
-import React, {useState} from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Pressable,
+    Switch
+} from 'react-native';
+import {useState} from "react";
 
-export default function BotonesScreen() {
+export default function App() {
+    const [accion, setAccion] = useState('nada');
+    const [isDisabled, setIsDisabled] = useState(false);
+
     return (
-        <View>
-            <Text>Próximamente by Rogelio y compañía</Text>
+        <View style={styles.container}>
+            <Text>
+                Activado:
+            </Text>
+            <Switch
+                onValueChange={() => setIsDisabled(!isDisabled)}
+                value={!isDisabled}
+            />
+
+            <Text>
+                { accion }
+            </Text>
+
+            <Pressable
+                onPressIn={() => setAccion('Pressable In')}
+                onPressOut={() => setAccion('Pressable Out')}
+                onLongPress={() => setAccion('Pressable Long')}
+                disabled={isDisabled}
+            >
+                {({pressed}) => (
+                    <View style={[styles.card, !pressed && styles.shadow]}>
+                        <View style={styles.mockImage}/>
+                        <Text>
+                            { pressed ? 'Tarjeta presionada.' : 'Tarjeta no presionada.' }
+                        </Text>
+                    </View>
+                )}
+            </Pressable>
+            <TouchableOpacity
+                activeOpacity={0.2}
+                onPress={() => setAccion('Opacity')}
+                disabled={isDisabled}
+            >
+                <View style={[styles.card, styles.shadow]}>
+                    <View style={styles.mockImage}/>
+                    <Text>
+                        Esta es una tarjeta.
+                    </Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableWithoutFeedback
+                disabled={isDisabled}
+                onPress={() => setAccion('Without Feedback')}
+            >
+                <View style={[styles.card, styles.shadow]}>
+                    <View style={styles.mockImage}/>
+                    <Text>
+                        Esta es una tarjeta.
+                    </Text>
+                </View>
+            </TouchableWithoutFeedback>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +75,27 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    card: {
+        width: 200,
+        height: 250,
+        padding: 20,
+        display: "flex",
+        flexDirection: 'column',
+        borderRadius: 8,
+        margin: 15
+    },
+    mockImage: {
+        flex: 1,
+        backgroundColor: 'gray',
+        marginBottom: 8,
+        borderRadius: 8
+    },
+    shadow: {
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 10},
+        shadowOpacity: 0.2,
+        shadowRadius: 12,
+        elevation: 10,
     },
 });
